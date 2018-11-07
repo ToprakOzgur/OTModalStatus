9cfcbf991eb611a3128a9722a2b798021f78f52f
ToprakOzgur <zgrtprk@gmail.com>
2018-11-07 22:12:19 +0300

    first

---
+++ OTModalStatus/OTModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  OTModalStatus.h
+//  OTModalStatus
+//
+//  Created by Ozgur Toprak on 6.11.2018.
+//  Copyright © 2018 Ozgur Toprak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OTModalStatus.
+FOUNDATION_EXPORT double OTModalStatusVersionNumber;
+
+//! Project version string for OTModalStatus.
+FOUNDATION_EXPORT const unsigned char OTModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTModalStatus/PublicHeader.h>
+
+
